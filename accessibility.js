b02839123a5ac1d496e03cda4ebc9bb1effb0b9f
likeusb <80967503+likeusb@users.noreply.github.com>
2024-08-10 10:34:45 +0300

    Add files via upload

--- accessibility.js
+++ accessibility.js
@@ -1,37 +1,59 @@
+var r = document.querySelector(':root');
 // r.style.setProperty('--bg-prim', rgbValue);
+// document.cookie = "--bg-prim=; expires=Thu, 01 Jan 1970 00:00:00 UTC; SameSite=Lax;";
 document.addEventListener('DOMContentLoaded', function() {  
-    var button1 = document.getElementById('submit1');
-    var button2 = document.getElementById('retrieve');
-    var button3 = document.getElementById('reset');
-    var r = document.querySelector(':root');
+    var submit = document.getElementById('submit');
+    var reset = document.getElementById('reset');
 
-    button1.addEventListener('click', rgbValues);
+    var lightMode = document.getElementById('lightMode');
+    var grayMode = document.getElementById('grayMode');
 
-    button2.addEventListener('click', function() {
-        console.log(getCookie('--bg-prim'));
+    submit.addEventListener('click', rgbValues);
+
+    reset.addEventListener('click', function() {
+        updateThemeSpecific('rgb(0, 0, 0)', 'rgb(20, 20, 20)', 'rgb(40, 40, 40)', 'rgb(87, 87, 87)', 'white', 'rgb(50, 115, 255)', 'rgb(255, 0, 0)');
     });
 
-    
+    lightMode.addEventListener('click', function() {
+        updateThemeSpecific('white', 'rgb(240, 240, 240)', 'rgb(235, 235, 235)', 'rgb(185, 185, 185)', 'black', 'rgb(0, 59, 185)', 'rgb(255, 0, 0)');
+    });
+
+    grayMode.addEventListener('click', function() {
+        updateThemeSpecific('rgb(30, 30, 30', 'rgb(20, 20, 20)', 'rgb(10, 10, 10)', 'rgb(121, 121, 121)', 'white', 'rgb(58, 119, 252)', 'rgb(255, 0, 0)');
+    });
     
     function rgbValues() {
-        const rgb = document.getElementById('bg1');
-
         const pickers = document.querySelectorAll("input.color");
 
         values = [pickers[0].value, pickers[1].value, pickers[2].value, pickers[3].value, pickers[4].value, pickers[5].value, pickers[6].value];
 
         setCookie('--bg-prim', values);
 
-        r.style.setProperty('--bg-prim', values[0]);
-        r.style.setProperty('--bg-sec', values[1]);
-        r.style.setProperty('--bg-tert', values[2]);
-        r.style.setProperty('--border-prim', values[3]);
-        r.style.setProperty('--fc-prim', values[4]);
-        r.style.setProperty('--acc', values[5]);
-        r.style.setProperty('--warn', values[6]);
+        updateTheme(values);
     }
-    
 });
+
+function updateThemeSpecific(color1, color2, color3, color4, color5, color6, color7) {
+    r.style.setProperty('--bg-prim', color1);
+    r.style.setProperty('--bg-sec', color2);
+    r.style.setProperty('--bg-tert', color3);
+    r.style.setProperty('--border-prim', color4)
+    r.style.setProperty('--fc-prim', color5);
+    r.style.setProperty('--acc', color6);
+    r.style.setProperty('--warn', color7);
+
+    setCookie('--bg-prim', color1 + ',' + color2 + ',' + color3 + ',' + color4 + ',' + color5 + ',' + color6 + ',' + color7);
+}
+
+function updateTheme(values) {
+    r.style.setProperty('--bg-prim', values[0]);
+    r.style.setProperty('--bg-sec', values[1]);
+    r.style.setProperty('--bg-tert', values[2]);
+    r.style.setProperty('--border-prim', values[3]);
+    r.style.setProperty('--fc-prim', values[4]);
+    r.style.setProperty('--acc', values[5]);
+    r.style.setProperty('--warn', values[6]);
+}
 
 function setCookie(name, value) {
     const d = new Date();
@@ -46,3 +68,8 @@ function getCookie(name) {
     const parts = value.split("; " + name + "=");
     if (parts.length == 2) return parts.pop().split(";").shift();
 }
+
+window.onload = function() {
+    const theme = getCookie('--bg-prim').split(',');
+    updateTheme(theme);
+};
